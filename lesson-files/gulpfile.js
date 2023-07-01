2e7993ef30ffc0f131ff2b6bf58f8e2d16885f5b
Fedor Sherbanyuk <fedor.sherbanyuk1@gmail.com>
2023-07-01 16:34:54 +0300

    hw61

--- lesson-files/gulpfile.js
+++ lesson-files/gulpfile.js
@@ -1,52 +1,31 @@
-const { parallel, task, watch } = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+const browserSync  = require('browser-sync').create();
 
-function squareRectangle(firstSideOfTheRectangle, secondSideOfTheRectangle) {
-    const result = firstSideOfTheRectangle * secondSideOfTheRectangle;
-    console.log('Square of rectangle:', result);
-    return result;
-}
+gulp.task('sass',() =>{
+    return gulp.src('./src/scss/*.scss')
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
+});
 
-function squareParallelogram(sideLengthParallelogram, heightLengthParallelogram) {
-    const result = sideLengthParallelogram * heightLengthParallelogram;
-    console.log('Square of parallelogram:', result);
-    return result;
-}
+gulp.task('pug',() =>{
+    return gulp.src('./src/pug/*.pug').pipe(pug())
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream());
+});
 
-function squareRhombus(rhombusSideLength, rhombusHeightLength) {
-    const result = rhombusSideLength * rhombusHeightLength;
-    console.log('Square of rhombus:', result);
-    return result;
-}
+gulp.task('build',gulp.parallel('sass','pug'));
 
-task('default', parallel(
-    function() {
-        squareRectangle(5, 5);
-    },
-    function() {
-        squareParallelogram(6, 6);
-    },
-    function() {
-        squareRhombus(7, 7);
-    }
-));
-//watch
-function handleFileChange(event) {
-    console.log(`File ${event.path} was ${event.type}`);
+gulp.task('server',()=>{
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
+    })
+});
 
-}
-
-function handleDirectoryChange(event) {
-    console.log(`Directory ${event.path} was ${event.type}`);
-
-}
-
-function watchFiles() {
-    watch('src/**/*.css', handleFileChange);
-    watch('src/**/*.js', handleFileChange);
-    watch('src/images/**/*', handleFileChange);
-    watch('src/index.html', handleFileChange);
-    watch('src/', {events: 'all'}, handleDirectoryChange);
-}
-
-exports.default = watchFiles;
-
+gulp.watch('./src/scss/*.scss',gulp.series('sass'));
+gulp.watch('./src/pug/*.pug',gulp.series('pug'));
+gulp.watch('./dist/*.html').on('change', browserSync.reload);
+gulp.task('development', gulp.series('build', 'server'));
